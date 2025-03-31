107bb4af98354cc5761bb50b0c04b5b1b4cdcec7
DorianDum <dumarchedorian@gmail.com>
2025-03-31 16:58:36 +0200

    Add files via upload
    les class avec des trucs dedans

--- include/arret.hpp
+++ include/arret.hpp
@@ -1,10 +1,19 @@
 #ifndef ARRET
 #define ARRET
 
+#include<iostream>
+
+struct horaire{
+    unsigned int heure;
+    unsigned int minute;
+} typedef horaire;
 
 class arret{
     public:
-    
+    char* stop_id ; // unique a chaque arret
+    int *route_id ; //liste de toutes les lignes qui passe par cette arret 
+    horaire heure ;
+
 };
 
 #endif
--- include/trajet.hpp
+++ include/trajet.hpp
@@ -3,12 +3,17 @@
 
 #define N 3
 
-#include<iostream>
+#include "arret.hpp"
+
 using namespace std;
 
 class trajet{
     public:
-
+    int route_id;
+    int trip_id;
+    char *trip_headsign; //nom du terminus
+    int * stop; // une liste des stop_id liés à la class arret
+    int taille; //taille de la liste stop
 };
 
 #endif
